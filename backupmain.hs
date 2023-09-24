72c20c554a9cc5618b03380f75b8190cd5467287
dinho11701 <48062027+dinho11701@users.noreply.github.com>
2023-09-24 15:45:55 -0400

    Create backupmain.hs

---
+++ backupmain.hs
@@ -0,0 +1,80 @@
+import Move
+
+-- Définition d'un type de données pour la structure
+data Joueur = Joueur { alignement1 :: Int
+                     , alignement2 :: Int
+                     , listeVariables :: [Int] ---liste des Alignements
+                     , scoreTotal :: Int --score du joueur
+                     } deriving (Show)
+                     
+
+-- Fonction pour calculer le score comme la somme des alignements d'un joueur
+score :: Joueur -> Joueur
+score joueur = joueur { scoreTotal = sum (listeVariables joueur) }
+
+
+data PieceUser = Zero0 | Zero1 | Zero2 | Zero3 deriving (Eq,Show)
+
+data PieceComputer = X0 | X1 | X2 | X3 deriving (Eq,Show)
+
+transformerPieceUserEnInt :: PieceUser -> Int
+transformerPieceUserEnInt intUser = case intUser of
+    Zero0 -> 00
+    Zero1 -> 01
+    Zero2 -> 02
+    Zero3 -> 03
+
+
+-- Exemple d'utilisation
+main :: IO ()
+main = do
+    -- Création d'une instance de Joueur avec le score calculé
+    let joueur1 = score (Joueur { alignement1 = 10
+                                        , alignement2 = 20
+                                        , listeVariables = [10, 20]
+                                        , scoreTotal = 0
+                                        })
+
+    let jeu = [ ["03", "", "__", "XX"]
+                  , ["02", "X3", "__", "__"]
+                  , ["01", "__", "__", "__"]
+                  , ["AllahOuakbar", "__", "__", "__"] ]
+
+    --putStrLn ("Est-ce que " ++ show (transformerPieceUserEnInt Zero0 `elem` tableauJeu)
+    
+
+    -- Affichage des attributs de l'objet
+    putStrLn ("Variable 1 : " ++ show (alignement1 joueur1))
+    putStrLn ("Variable 2 : " ++ show (alignement2 joueur1))
+    putStrLn ("Liste de variables : " ++ show (listeVariables joueur1))
+    putStrLn ("Score du joueur : " ++ show (scoreTotal joueur1))
+
+    let listeMovRest1 = [listeBUser,listeMUser,listeSUser]
+    let listeMovRest2 = [listeBComp,listeMComp,listeSComp]
+
+    let new1 = formaterListeMov listeMovRest1
+    let new2 = formaterListeMov listeMovRest2
+
+    print jeu
+    putStrLn (new1 ++ " || " ++ new2)
+
+    
+    -- Fonction récursive pour parcourir le tableau
+    -- Fonction récursive pour parcourir le tableau
+    let afficheCaseTableau [] _ = return ()
+        afficheCaseTableau (ligne:reste) ligneIndex = do
+            parcourirLigne ligne 0 ligneIndex
+            afficheCaseTableau reste (ligneIndex + 1)
+    
+        parcourirLigne [] _ _ = return ()
+        parcourirLigne (caseValue:cases) colonneIndex ligneIndex
+            | colonneIndex /= ligneIndex = do
+                let (i, j) = (colonneIndex, ligneIndex) -- Inverser i et j
+                putStrLn ("Case (" ++ show i ++ "," ++ show j ++ ") : " ++ caseValue)
+                parcourirLigne cases (colonneIndex + 1) ligneIndex
+            | otherwise = do
+                putStrLn ("Case (" ++ show ligneIndex ++ "," ++ show colonneIndex ++ ") : " ++ caseValue)
+                parcourirLigne cases (colonneIndex + 1) ligneIndex
+    
+    -- Appel initial de la fonction de parcours
+    afficheCaseTableau jeu 0
