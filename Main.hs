db360ec03827d12c7469ae7d66e160a709d3362e
dinho11701 <48062027+dinho11701@users.noreply.github.com>
2023-09-25 00:00:43 -0400

    ajout version a corriger dans parcourirTab2D dans Main.hs

--- Main.hs
+++ Main.hs
@@ -49,7 +49,6 @@ faireCasDiag tab2D element j i = do
         then listePos
         else [-1, -1]
     else [-1, -1]
-
 parcourirTab2D :: [[String]] -> IO ()
 parcourirTab2D tab2D = do
   -- Parcourir le tableau en utilisant deux boucles for
@@ -60,6 +59,58 @@ parcourirTab2D tab2D = do
       let user = estUnUser element
       let num = 0
       print user
+
+      if user
+        then do
+          --casdroite
+          let casse = faireCasDroite tab2D element j i
+          if casse !! 0 == -1
+            then do
+              let casse1 = faireCasDroite tab2D element j i
+              -- Traitement pour casse1
+            else if casse /= [-1, -1]
+              then do
+                let num'' = incrementeNumPieceJoueur num'
+                let casse11 = faireCasBas tab2D element (j + 1) i
+                if casse11 !! 0 == -1
+                  then putStrLn "Alignement2"
+                  else putStrLn "Alignement3"
+              else putStrLn "gogo"
+
+          --casbas
+          let casse1 = faireCasBas tab2D element j i
+          if casse1 !! 0 == -1
+            then do
+              let casse2 = faireCasDiag tab2D element j i  -- Correction de la fonction appelée
+              let val1 = True
+            else if casse1 /= [-1, -1]
+              then do 
+                let num'' = incrementeNumPieceJoueur num'
+                let casse11 = faireCasBas tab2D element j (i + 1)
+                if casse11 !! 0 == -1
+                  then putStrLn "Alignement2"
+                  else putStrLn "Alignement3"
+              else putStrLn "chaud"
+
+          --casdiag
+          let val1 = True
+          if val1
+            then do
+              let casse2 = faireCasDiag tab2D element j i
+              if casse2 !! 0 == -1 
+                then do
+                  let val2 = True
+                else if casse2 /= [-1, -1] 
+                  then do 
+                    let num'' = incrementeNumPieceJoueur num'
+                    let casse22 = faireCasDiag tab2D element (j + 1) (i + 1)
+                    if casse22 !! 0 == -1
+                      then putStrLn "Alignement2"
+                      else putStrLn "Alignement3"
+              else putStrLn "gogo"
+        else putStrLn "f"
+
+{- 
       if user
         then do
           let num' = incrementeNumPieceJoueur num  -- Mise à jour de num
@@ -80,7 +131,7 @@ parcourirTab2D tab2D = do
               else putStrLn $ "L'utilisateur a la case (" ++ show j ++ ", " ++ show i ++ ") : " ++ show element
             else putStrLn $ "L'utilisateur a la case (" ++ show j ++ ", " ++ show i ++ ") : " ++ show element
         else putStrLn "Non"
-
+-}
 
 -- Exemple d'utilisation
 main :: IO ()
