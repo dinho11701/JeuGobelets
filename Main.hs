cf7f57960741d09871ff558ca70533abbed8a516
dinho11701 <48062027+dinho11701@users.noreply.github.com>
2023-09-25 09:39:56 -0400

    boucle infinie mais sur bonne voie dans Main.hs

--- Main.hs
+++ Main.hs
@@ -49,107 +49,83 @@ faireCasDiag tab2D element j i = do
         then listePos
         else [-1, -1]
     else [-1, -1]
-parcourirTab2D :: [[String]] -> IO ()
-parcourirTab2D tab2D = do
-  -- Parcourir le tableau en utilisant deux boucles for
+
+
+-- Création d'une liste vide
+casesInterdites :: [a]
+casesInterdites = []
+
+-- Vérification si une liste est vide
+estVide :: [a] -> Bool
+estVide [] = True
+estVide _  = False
+
+parcourirTab2D :: [[String]] -> [Int] -> IO ()
+parcourirTab2D tab2D maListe = do
+  --let maListe = casesInterdites
+  let vide = estVide maListe
+
   forM_ [0..(length tab2D - 1)] $ \i -> do
     forM_ [0..(length (tab2D !! 0) - 1)] $ \j -> do
+      if not vide
+        then do
+          let elem1 = (tab2D !! (maListe !! 0) !! (maListe !! 1))
+          let elem2 = (tab2D !! (maListe !! 2) !! (maListe !! 3))
+          if (tab2D !! j !! i) /= elem1 && (tab2D !! j !! i) /= elem2
+            then do 
+              putStrLn "YEEES INTERDIT"
+          else putStrLn "YEEES INTERDIT"
+      else putStrLn "vide"
+    
       let element = tab2D !! i !! j
       putStrLn $ "Élément à la position (" ++ show j ++ ", " ++ show i ++ ") : " ++ show element
+      
       let user = estUnUser element
       let num = 0
       print user
-{-
-      if user
-        then do
-          --casdroite
-          let casse = faireCasDroite tab2D element j i
-          if casse !! 0 == -1
-            then do
-              let casse1 = faireCasDroite tab2D element j i
-              -- Traitement pour casse1
-            else if casse /= [-1, -1]
-              then do
-                let num'' = incrementeNumPieceJoueur num'
-                let casse11 = faireCasBas tab2D element (j + 1) i
-                if casse11 !! 0 == -1
-                  then putStrLn "Alignement2"
-                  else putStrLn "Alignement3"
-              else putStrLn "gogo"
-
-          --casbas
-          let casse1 = faireCasBas tab2D element j i
-          if casse1 !! 0 == -1
-            then do
-              let casse2 = faireCasDiag tab2D element j i  -- Correction de la fonction appelée
-              let val1 = True
-            else if casse1 /= [-1, -1]
-              then do 
-                let num'' = incrementeNumPieceJoueur num'
-                let casse11 = faireCasBas tab2D element j (i + 1)
-                if casse11 !! 0 == -1
-                  then putStrLn "Alignement2"
-                  else putStrLn "Alignement3"
-              else putStrLn "chaud"
-
-          --casdiag
-          let val1 = True
-          if val1
-            then do
-              let casse2 = faireCasDiag tab2D element j i
-              if casse2 !! 0 == -1 
-                then do
-                  let val2 = True
-                else if casse2 /= [-1, -1] 
-                  then do 
-                    let num'' = incrementeNumPieceJoueur num'
-                    let casse22 = faireCasDiag tab2D element (j + 1) (i + 1)
-                    if casse22 !! 0 == -1
-                      then putStrLn "Alignement2"
-                      else putStrLn "Alignement3"
-              else putStrLn "gogo"
-        else putStrLn "f"
--}
 
       if user
         then do
-          let num' = incrementeNumPieceJoueur num  -- Mise à jour de num
+          let num' = incrementeNumPieceJoueur num
           let casse = faireCasDroite tab2D element j i
           if casse !! 0 == -1
             then do
-              let casse1 = faireCasBas tab2D element j i  -- Correction de la fonction appelée
+              let casse1 = faireCasBas tab2D element j i
               if casse1 !! 0 == -1
                 then do
-                  let casse2 = faireCasDiag tab2D element j i  -- Correction de la fonction appelée
-                  --let val1 = True
+                  let casse2 = faireCasDiag tab2D element j i
                   if casse2 !! 0 == -1
                     then putStrLn "Rien à proximité"
-                  else do
-                    let num'' = incrementeNumPieceJoueur num'  -- Incrémentation de num
-                    let casse22 = faireCasDiag tab2D element (j + 1) (i + 1)
-                    if casse22 !! 0 == -1
-                      then putStrLn "Alignement2"
-                      else putStrLn "Alignement3"
-                    putStrLn $ "Valeur de num' mise à jour : " ++ show num''
-
+                    else do
+                      let num'' = incrementeNumPieceJoueur num'
+                      let casse22 = faireCasDiag tab2D element (j + 1) (i + 1)
+                      if casse22 !! 0 == -1
+                        then putStrLn "Alignement2"
+                        else do
+                          putStrLn "Alignement3"
+                          let maListe = [casse22 !! 0, casse22 !! 1, j + 1, i + 1]
+                          putStrLn $ "Valeur de num' mise à jour : " ++ show num''
+                          print maListe
+                          print (tab2D !! (maListe !! 0) !! (maListe !! 1))
+                          print (tab2D !! (maListe !! 2) !! (maListe !! 3))
+                          parcourirTab2D tab2D maListe
               else if casse1 /= [-1, -1]
-              then do 
-                let num'' = incrementeNumPieceJoueur num'
-                let casse11 = faireCasBas tab2D element j (i + 1)
-                if casse11 !! 0 == -1
-                  then putStrLn "Alignement2"
-                  else putStrLn "Alignement3"
-              else putStrLn "chaud"
+                then do 
+                  let num'' = incrementeNumPieceJoueur num'
+                  let casse11 = faireCasBas tab2D element j (i + 1)
+                  if casse11 !! 0 == -1
+                    then putStrLn "Alignement2"
+                    else putStrLn "Alignement3"
+                else putStrLn "chaud"
           else if casse /= [-1, -1]
-              then do
-                let num'' = incrementeNumPieceJoueur num'
-                let casse11 = faireCasBas tab2D element (j + 1) i
-                if casse11 !! 0 == -1
-                  then putStrLn "Alignement2"
-                  else putStrLn "Alignement3"
-              else putStrLn "gogo"
-      else putStrLn "Non"
-
+            then do
+              let num'' = incrementeNumPieceJoueur num'
+              let casse11 = faireCasBas tab2D element (j + 1) i
+              if casse11 !! 0 == -1
+                then putStrLn "Alignement2"
+                else putStrLn "Alignement3"
+            else putStrLn "gogo"
+        else putStrLn "Non"
 
 -- Exemple d'utilisation
 main :: IO ()
@@ -158,10 +134,12 @@ main = do
 
   let liste = ["X3","X2","X1"]
 
+  let list1 = []
+
   let estUser = estUnUser "0"
   putStrLn (show estUser)
 
-  parcourirTab2D jeu1
+  parcourirTab2D jeu1 list1
   
   
   
