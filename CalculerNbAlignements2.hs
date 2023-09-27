137d792f1ff5c9ab3917e5cbe3f15c9ea1c43a6e
dinho11701 <48062027+dinho11701@users.noreply.github.com>
2023-09-27 11:44:38 -0400

    ajout modif pour gerer cas ou ya 1 ligne ds 3 directions dans CalculerNbAlignements2.hs

--- CalculerNbAlignements2.hs
+++ CalculerNbAlignements2.hs
@@ -10,14 +10,13 @@ calculerAlignements (x1 : y1 : x2 : y2 : xs) =
     
 
 
-
-
-
 estUneLigne :: Int -> Int -> Int -> Int -> Bool
 estUneLigne x1 y1 x2 y2 =
   (estUneLigneXDroite x1 x2 && estUneLigneYDroite y1 y2)
     || (estUneLigneXBas x1 x2 && estUneLigneYBas y1 y2)
-    || (estUneLigneDiagonale x1 x2 y1 y2)
+    || (estUneLigneDiagonale x1 x2 y1 y2) || (estUneLigneXDroite x1 x2 && estUneLigneYDroite y1 y2)
+    && (estUneLigneXBas x1 x2 && estUneLigneYBas y1 y2)
+    && (estUneLigneDiagonale x1 x2 y1 y2) 
 
 estUneLigneXDroite :: Int -> Int -> Bool
 estUneLigneXDroite x1 x2 = x1 + 1 == x2
@@ -36,10 +35,15 @@ estUneLigneDiagonale x1 x2 y1 y2 = x1 + 1 == x2 && y1 + 1 == y2
 
 main :: IO ()
 main = do
-  let tableau = [0,0, 1,1, 0,1 ,0,2 ,0,3]  -- Remplacez par votre propre tableau d'entiers
+  let tableau = [0,0, 1,1, 0,1 ,0,2 ,0,3]  -- Remplacez par votre propre tableau d'entiers ici alignements de 2
   
-  let tab = [0,0,1,1,1,0]  
+  let tab = [0,0,1,1,1,0,0,1]  --bon
+  
+  let tab1 = [0,0 ,1,0, 3,0 ,0,1 ,1,1, 2,2 , 0,3, 1,3 ,3,3]-- pour l'instant on va dire que cest bon
 
-  let alignements = calculerAlignements tab
+  
+  let tab11score = [0,0,1,0,2,0,3,0,0,1,0,2,0,3,1,3,2,3,3,3,1,2,2,2,2,3,1,1,1,2,1,3]
+  
+  let alignements = calculerAlignements tab1
 
   putStrLn $ "Nombre d'alignements : " ++ show alignements
